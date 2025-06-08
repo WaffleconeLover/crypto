75f317a383bbd6641af9d9e733f4ae1622a7c842
Wafflecone <7074360+WaffleconeLover@users.noreply.github.com>
2025-06-07 17:04:35 -0700

    Update ETH Liquidity Band Dashboard.py

--- pages/ETH Liquidity Band Dashboard.py
+++ pages/ETH Liquidity Band Dashboard.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import mplfinance as mpf
 from datetime import datetime
 import gspread
+import json
 from google.oauth2.service_account import Credentials
 
 st.set_page_config(layout="wide")
@@ -49,7 +50,8 @@ def compute_heikin_ashi(df):
 
 def load_google_sheet_text(sheet_id, tab_name="BandSetup", cell_range="B14:B17"):
     scope = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
-    creds = Credentials.from_service_account_file("creds.json", scopes=scope)
+    creds_dict = json.loads(st.secrets["google_service_account"])
+    creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
     gc = gspread.authorize(creds)
     worksheet = gc.open_by_key(sheet_id).worksheet(tab_name)
     cells = worksheet.get(cell_range)
